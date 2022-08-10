b415ee436b426968e3dba225db9064a38d79d735
Joji <44543279+510r@users.noreply.github.com>
2022-08-10 23:46:21 +0200

    Update check_updates.sh

--- check_updates.sh
+++ check_updates.sh
@@ -1,15 +1,31 @@
 #!/usr/bin/env bash
+# From https://github.com/kvz/bash3boilerplate
+# Require at least bash 3.x
+if [[ "${BASH_VERSINFO[0]}" -lt "3" ]]; then echo "bash version < 3"; exit 1; fi
+
+# Exit on error. Append || true if you expect an error.
+set -o errexit
+set -o nounset
+
+# Bash will remember and return the highest exit code in a chain of pipes.
+set -o pipefail
 
 PATH=/bin:/sbin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
 
 timestamp_file="/run/zabbix/caendra_check_update"
 update_interval="86400" # 1 day
 timestamp_file_mtime="0"
-os="centos"
+os=""
 epoch=$(date "+%s")
 tmpfile=$( mktemp --tmpdir=/run/zabbix )    
 outfile="/run/zabbix/zabbix.count.updates"
 
+function _detectOS {
+    if [[ -e /etc/centos-release ]]; then
+        export os="centos"
+    fi
+    if [[ -e /etc/debian_version ]]; then
+        export os="debian"
     fi
 }
 
@@ -30,41 +46,40 @@ function _check_last_update {
 
 
 function _check_OS_upgrades {
-
-if [[ "$os" == "centos" ]]; then
-        if [[ ! -e /var/cache/yum/x86_64/7/base/repomd.xml ]]; then
-            # if the repomd.xml file does not exists,
-            # we assume that this is a new machine
-            # or "yum clean all" was run
-            export update_needed="y"
-        fi
-
+    if [[ "$os" == "debian" ]]; then 
         if [[ "$update_needed" == "y" ]]; then
-            # forced true as the --assumeno option
-            # always returns exit code 1
-            yum updateinfo list security all --assumeno &> /dev/null || true
+            apt update &>/dev/null
             touch $timestamp_file
         fi
 
-        yum_output=$(yum updateinfo list security all --cacheonly && rc=$? || rc=$?; echo "rc=$rc" > $tmpfile)
+        pkg_to_update=$((apt-get upgrade --simulate 2>&1 | wc -l) || true)
+    fi
+if [[ "$os" == "centos" ]]; then
+        if [[ ! -e /var/cache/yum/x86_64/7/base/repomd.xml ]]; then
+            # if the repomd.xml file does not exists,
+            # we assume that this is a new machine 
+            # or "yum clean all" was run
+            export update_needed="y"
+        fi
+        if [[ "$update_needed" == "y" ]]; then
+            # forced true as the --assumeno option 
+            # always returns exit code 1
+            yum upgrade --assumeno &> /dev/null || true
+            touch $timestamp_file
+        fi
+        yum_output=$(yum check-update --cacheonly && rc=$? || rc=$?; echo "rc=$rc" > $tmpfile)
         source $tmpfile
         rm $tmpfile
-
         if [[ "$rc" == "0" ]]; then
             pkg_to_update="0"
         fi
-
          if [[ "$rc" == "100" ]]; then
             pkg_to_update=$(echo "$yum_output" | egrep -v '^(Load| \*|$)' | wc -l)
         fi
     fi
 }
-
-
+_detectOS
 _check_last_update
-
 pkg_to_update=""
-
 _check_OS_upgrades
-
 echo "$pkg_to_update" > $outfile
