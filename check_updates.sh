5b7925ddf7ac9a4ce1287908f38633c22763d1e9
Joji <44543279+510r@users.noreply.github.com>
2022-08-10 23:38:06 +0200

    Add files via upload

--- check_updates.sh
+++ check_updates.sh
@@ -1,31 +1,15 @@
 #!/usr/bin/env bash
-# From https://github.com/kvz/bash3boilerplate
-# Require at least bash 3.x
-if [[ "${BASH_VERSINFO[0]}" -lt "3" ]]; then echo "bash version < 3"; exit 1; fi
-
-# Exit on error. Append || true if you expect an error.
-set -o errexit
-set -o nounset
-
-# Bash will remember and return the highest exit code in a chain of pipes.
-set -o pipefail
 
 PATH=/bin:/sbin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
 
 timestamp_file="/run/zabbix/caendra_check_update"
 update_interval="86400" # 1 day
 timestamp_file_mtime="0"
-os=""
+os="centos"
 epoch=$(date "+%s")
 tmpfile=$( mktemp --tmpdir=/run/zabbix )    
 outfile="/run/zabbix/zabbix.count.updates"
 
-function _detectOS {
-    if [[ -e /etc/centos-release ]]; then
-        export os="centos"
-    fi
-    if [[ -e /etc/debian_version ]]; then
-        export os="debian"
     fi
 }
 
@@ -46,31 +30,23 @@ function _check_last_update {
 
 
 function _check_OS_upgrades {
-    if [[ "$os" == "debian" ]]; then 
-        if [[ "$update_needed" == "y" ]]; then
-            apt update &>/dev/null
-            touch $timestamp_file
-        fi
-
-        pkg_to_update=$((apt-get upgrade --simulate 2>&1 | wc -l) || true)
-    fi
 
 if [[ "$os" == "centos" ]]; then
         if [[ ! -e /var/cache/yum/x86_64/7/base/repomd.xml ]]; then
             # if the repomd.xml file does not exists,
-            # we assume that this is a new machine 
+            # we assume that this is a new machine
             # or "yum clean all" was run
             export update_needed="y"
         fi
 
         if [[ "$update_needed" == "y" ]]; then
-            # forced true as the --assumeno option 
+            # forced true as the --assumeno option
             # always returns exit code 1
-            yum upgrade --assumeno &> /dev/null || true
+            yum updateinfo list security all --assumeno &> /dev/null || true
             touch $timestamp_file
         fi
 
-        yum_output=$(yum check-update --cacheonly && rc=$? || rc=$?; echo "rc=$rc" > $tmpfile)
+        yum_output=$(yum updateinfo list security all --cacheonly && rc=$? || rc=$?; echo "rc=$rc" > $tmpfile)
         source $tmpfile
         rm $tmpfile
 
@@ -85,7 +61,6 @@ if [[ "$os" == "centos" ]]; then
 }
 
 
-_detectOS
 _check_last_update
 
 pkg_to_update=""
